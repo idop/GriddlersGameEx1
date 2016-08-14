bb6e317e1dfa9788cd43ffffeaa417101b353eab
Amitai Handler <amitaihandler@gmail.com>
2016-08-14 23:40:42 +0300

    added menu items

--- UI/src/MainMenu.java
+++ UI/src/MainMenu.java
@@ -6,13 +6,19 @@ public class MainMenu {
     private static void draw()
     {
         Scanner in = new Scanner(System.in);
-        System.out.println("Welcome to the Griddler");
+
         int selection;
 
         do
         {
-            System.out.println("[1] New Game");
-            System.out.println("[2] Quit");
+            System.out.println("[1] Provide configuration file path");
+            System.out.println("[2] Start Game");
+            System.out.println("[3] Show board state");
+            System.out.println("[4] Make move");
+            System.out.println("[5] Show moves history");
+            System.out.println("[6] Undo last move");
+            System.out.println("[7] Get game statistics");
+            System.out.println("[8] Quit game");
             selection = in.nextInt();
 
             switch(selection)
@@ -21,16 +27,30 @@ public class MainMenu {
                     break;
                 case 2:
                     break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
                 default:
                     break;
             }
 
-        }while (selection != 3);
+        }while (selection != 8);
     }
 
 
     public static void main(String[] args)
     {
+        System.out.println("Welcome to the Griddler!");
+        System.out.println("------------------------");
         draw();
     }
 
