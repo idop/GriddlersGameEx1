77d8e352e8610cd03a305a16803c160460f0b7cc
Amitai Handler <amitaihandler@gmail.com>
2016-08-17 00:46:34 +0300

    added GameView class to hold the needed information (board and constraints) to display the game status

---
+++ UI/src/GameView.java
@@ -0,0 +1,22 @@
+/**
+ * Created by amitaihandler on 8/16/16.
+ */
+import Game.*;
+
+public class GameView
+{
+    private Character[][] board;
+    private int rows;
+    private int columns;
+
+    public GameView(Game gGame)
+    {
+        //this.rows = gGame.gameBoard.getRows() + gGame.rowConstraints.
+
+    }
+
+    public void printGameBoardView()
+    {
+
+    }
+}
