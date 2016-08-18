64d3f40060ae5f602e514048a21cea49cce8e215
Amitai Handler <amitaihandler@gmail.com>
2016-08-18 22:50:46 +0300

    started the view

--- UI/src/GameView.java
+++ UI/src/GameView.java
@@ -5,19 +5,54 @@ import Game.*;
 
 public class GameView
 {
+    private static final int undecided = 63;
+    private static final int empty = 88;
+    private static final int black = 254;
     private Character[][] board;
+    private Game gGame;
     private int rows;
     private int columns;
+    private int actualRows;
+    private int actualColumns;
 
-    public GameView(Game gGame)
+
+    public GameView(Game i_Game)
     {
-        //*2 to leave space between constraints
-        this.rows       = gGame.gameBoard.getRows() + gGame.getMaxRowConstraints()*2;
-        this.columns    = gGame.gameBoard.getColumns() + gGame.getMaxColConstraints()*2;
+        gGame           = i_Game;
+        actualRows      = gGame.gameBoard.getRows();
+        actualColumns   = gGame.gameBoard.getColumns();
+        rows            = actualRows + gGame.getMaxRowConstraints() * 2;        //*2 to leave space between constraints
+        columns         = actualColumns + gGame.getMaxColConstraints() * 2;     //*2 to leave space between constraints
+        board           = new Character[rows][columns];
+        resetBoard();
     }
 
     public void printGameBoardView()
     {
+
+    }
+
+    public void resetBoard()
+    {
+        // resets the "actual board" to '?'
+        for (int i = rows - actualRows; i < rows; i++)
+        {
+            for (int j = columns - actualColumns; j < actualColumns; j++)
+            {
+                board[i][j] = (char)undecided;
+            }
+        }
+
+        // fill in the row constraints
+        for (int i = rows - actualRows; i < rows; i++)
+        {
+            for (int j = columns - actualColumns; j >= 0; j = j - 2)                // leave space between constraints
+            {
+
+            }
+        }
+
+        // fill in the column constraints
 
     }
 }
