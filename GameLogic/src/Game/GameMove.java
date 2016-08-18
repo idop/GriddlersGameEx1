68f83cc0f702cd7e30f810ed5a57e1da63ab5fd0
Amitai Handler <amitaihandler@gmail.com>
2016-08-18 22:51:11 +0300

    game move object

--- GameLogic/src/Game/GameMove.java
+++ GameLogic/src/Game/GameMove.java
@@ -7,25 +7,6 @@ public class GameMove
 {
     private int row;
     private int column;
-    public MoveDirection direction;
+    //private char
 
-    public enum MoveDirection
-    {
-        Horizontal,
-        Diagonal
-    }
-
-    public GameMove(int i_row, int i_column, int i_direction)
-    {
-        row     = i_row;
-        column  = i_column;
-        if (i_direction == 0)
-        {
-           direction =  MoveDirection.Horizontal;
-        }
-        else if (i_direction == 1)
-        {
-            direction = MoveDirection.Diagonal;
-        }
-    }
 }
