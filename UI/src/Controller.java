9d754c334eb2ddcd6e81d2e91548dcb8fb7ae72e
ido <ido.perry@gmail.com>
2016-09-10 14:21:41 +0300

    Load file is complete + binding the contorller from fxml

--- UI/src/Controller.java
+++ UI/src/Controller.java
@@ -1,12 +1,16 @@
+import GameXmlParser.GameBoardXmlParser;
+import GameXmlParser.GameDefinitionsXmlParserException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
-/**
- * Created by amitaihandler on 9/6/16.
- */
+import java.io.File;
+
 public class Controller {
 
     @FXML
@@ -17,5 +21,87 @@ public class Controller {
 
     @FXML
     private Button loadPuzzleBtn;
+
+    @FXML
+    private Button startGameBtn;
+
+    @FXML
+    private AnchorPane playPanel;
+
+    @FXML
+    private Label currentPlayerScore;
+
+    @FXML
+    private Label currentPlayerName;
+
+    @FXML
+    private Button playerClearSelectionBtn;
+
+    @FXML
+    private Button playerUndoLastBtn;
+
+    @FXML
+    private Button playerMakeMoveBtn;
+
+    @FXML
+    private TitledPane playerMoveHistory;
+
+    @FXML
+    private TitledPane playerStatistics;
+
+    @FXML
+    private AnchorPane boardView;
+
+    @FXML
+    private AnchorPane playersDisplay;
+
+    @FXML
+    private TableView<?> playersTableViewList;
+
+    private Stage primaryStage;
+    private GameBoardXmlParser gameXmlParser;
+    private FileChooser fileChooser = new FileChooser();
+
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+    @FXML
+    private void initialize() {
+        startGameBtn.setDisable(true);
+        playerClearSelectionBtn.setDisable(true);
+        playerUndoLastBtn.setDisable(true);
+        playerMakeMoveBtn.setDisable(true);
+    }
+
+    @FXML
+    void loadXmlFile(ActionEvent event) {
+        fileChooser.setTitle("Select Game Xml");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("xml files", "*.xml"));
+        File selectedFile = fileChooser.showOpenDialog(primaryStage);
+        if (selectedFile == null) {
+            return;
+        }
+
+        String absolutePath = selectedFile.getAbsolutePath();
+        fileChooser.setInitialDirectory(selectedFile.getParentFile());
+        try {
+            gameXmlParser = new GameBoardXmlParser(absolutePath);
+            puzzleFilePath.textProperty().setValue(absolutePath);
+            startGameBtn.setDisable(false);
+        } catch (GameDefinitionsXmlParserException e) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText("Provided game xml file is not valid");
+            alert.setContentText(e.getMessage());
+            alert.showAndWait();
+        }
+
+    }
+
+    @FXML
+    void startGame(ActionEvent event) {
+
+    }
 
 }
--- UI/src/Main.java
+++ UI/src/Main.java
@@ -1,19 +1,15 @@
 /* Created by Amitai Handler on 8/14/16. */
 
 
-import Game.BoardSquare;
 import Game.Game;
-import Game.PlayerTurn;
 import GameXmlParser.GameBoardXmlParser;
-import GameXmlParser.Schema.Constraint;
-import GameXmlParser.Schema.Constraints;
-
-import java.net.URL;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.net.URL;
 
 
 public class Main extends Application {
@@ -21,31 +17,26 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         String sceneFile = "GriddlersGame.fxml";
-        //Parent root = null;
-        URL url  = null;
-        try
-        {
-            url  = getClass().getResource( sceneFile );
-            Parent root = FXMLLoader.load( url );
-            System.out.println( "  fxmlResource = " + sceneFile );
-            primaryStage.setTitle("Griddlers Game!");
-            primaryStage.setScene(new Scene(root));
-            primaryStage.show();
-        }
-        catch ( Exception ex )
-        {
-            System.out.println( "Exception on FXMLLoader.load()" );
-            System.out.println( "  * url: " + url );
-            System.out.println( "  * " + ex );
-            System.out.println( "    ----------------------------------------\n" );
-            throw ex;
-        }
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        URL url = getClass().getResource(sceneFile);
+        fxmlLoader.setLocation(url);
+        Parent root = fxmlLoader.load(url.openStream());
+
+        Controller controller = fxmlLoader.getController();
+        controller.setPrimaryStage(primaryStage);
+
+        System.out.println("  fxmlResource = " + sceneFile);
+        primaryStage.setTitle("Griddlers Game!");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
+
     private static final String GAME_STARTED_MESSAGE = "Game Already Started";
     private GameBoardXmlParser parser = null;
     private Game game = null;
     boolean gameStarted = false;
     private boolean playerWon = false;
+
     public GameBoardXmlParser getParser() {
         try {
             String path = InputManager.getPath();
